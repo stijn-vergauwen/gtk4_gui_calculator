59981ee7c7f9fe3c6a76b396baf77912dcc57389
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-07-10 12:22:03 +0200

    make functionality to display entered digits

--- src/calculator_app/actions.rs
+++ src/calculator_app/actions.rs
@@ -1,3 +1,6 @@
+use gtk4::Label;
+
+
 #[derive(Clone, Copy)]
 pub enum CalculatorAction {
     Digit(u8),
@@ -14,11 +17,17 @@ pub enum Operator {
     Divide,
 }
 
-pub fn handle_action(action: CalculatorAction) {
+pub fn handle_action(action: CalculatorAction, display_label: &Label) {
     match action {
-        CalculatorAction::Digit(_) => println!("display digit"),
+        CalculatorAction::Digit(digit) => enter_digit(digit, display_label),
         CalculatorAction::Decimal => println!("display decimal"),
         CalculatorAction::Operator(_) => println!("do operator"),
         CalculatorAction::Equals => println!("calculate result"),
     }
+}
+
+fn enter_digit(digit: u8, display_label: &Label) {
+    let new_input = format!("{}{}", display_label.text(), digit);
+
+    display_label.set_text(&new_input);
 }
--- src/gui.rs
+++ src/gui.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use gtk4::prelude::*;
 use gtk4::{Application, ApplicationWindow, Button, Grid, Label, Orientation};
 use gui_calculator::calculator_app::actions::handle_action;
@@ -12,10 +14,10 @@ pub fn build_ui(application: &Application) {
     grid.set_column_homogeneous(true);
     grid.set_orientation(Orientation::Vertical);
 
-    let display_label = Label::new(Some("0"));
-    grid.attach(&display_label, 0, 0, 4, 1);
+    let display_label = Rc::new(Label::new(Some("0")));
+    grid.attach(display_label.as_ref(), 0, 0, 4, 1);
 
-    add_buttons_to_grid(&grid);
+    add_buttons_to_grid(&grid, display_label.clone());
 
     let window = ApplicationWindow::builder()
         .application(application)
@@ -28,7 +30,7 @@ pub fn build_ui(application: &Application) {
     window.present();
 }
 
-pub fn add_buttons_to_grid(grid: &Grid) {
+pub fn add_buttons_to_grid(grid: &Grid, display: Rc<Label>) {
     let buttons = get_buttons_for_calculator();
 
     for (index, button) in buttons.iter().enumerate() {
@@ -36,9 +38,12 @@ pub fn add_buttons_to_grid(grid: &Grid) {
         let row = f64::floor(index as f64 / 4f64) as i32 + 1;
 
         let action = button.action.clone();
+        
+        // Idk how it works, but using Rc<> allowed the display reference to be used in 'static closure
+        let cloned_display = display.clone();
 
         let button_object = Button::with_label(&button.label);
-        button_object.connect_clicked(move |_| handle_action(action));
+        button_object.connect_clicked(move |_| handle_action(action, cloned_display.as_ref()));
 
         grid.attach(&button_object, column, row, 1, 1);
     }
